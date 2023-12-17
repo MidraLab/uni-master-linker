27c0dca8b667b5035386a28e7b2fedb9ad264271
yousan <ka1357amnbpdr@gmail.com>
2023-12-17 10:41:57 +0900

    feat: データオブジェクトの作成用クラスの作成

---
+++ Assets/UniMasterLinker/Scripts/Util/CreateDataObject.cs
@@ -0,0 +1,53 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace UniMasterLinker.Util
+{
+    /// <summary>
+    /// データオブジェクトを作成するクラス
+    /// </summary>
+    public static class CreateDataObject
+    {
+        /// <summary>
+        /// データオブジェクトのクラスの作成パス
+        /// </summary>
+        private const string DataRootPath = "/UniMasterLinker/DataObject/";
+
+        /// <summary>
+        /// データオブジェクトのクラスを作成する
+        /// </summary>
+        public static void CreateDataObjectFiles(params string[] dataObjectNameList)
+        {
+            foreach (var dataObjectName in dataObjectNameList)
+            {
+                var objectName = dataObjectName + "DataObject";
+                if (IsExistDataObject(objectName)) continue;
+                CrateDataObject(objectName);
+            }
+        }
+
+        /// <summary>
+        /// データオブジェクトを作成
+        /// </summary>
+        /// <param name="dataObjectName"></param>
+        private static void CrateDataObject(string dataObjectName)
+        {
+            var obj = ScriptableObject.CreateInstance(dataObjectName);
+            var path = "Assets" + DataRootPath + dataObjectName + ".asset";
+            AssetDatabase.CreateAsset(obj, path);
+        }
+        
+        /// <summary>
+        /// データオブジェクトが存在するか
+        /// </summary>
+        /// <param name="dataObjectName"></param>
+        private static bool IsExistDataObject(string dataObjectName)
+        {
+            var path = Application.dataPath + DataRootPath + dataObjectName + ".asset";
+            return System.IO.File.Exists(path);
+        }
+    }
+}
+#endif
